0a0c77674dafadd8f4d7eb2ff7c25ce8a5736619
Jie Gao <jg3526@vanilla.clic.cs.columbia.edu>
2015-09-27 10:31:37 -0400

    get search result from Bing in json format, and extract the 3 fields matter, which are title, url, desp; next step should be getting user feedback, if no relevant confirmed, exit the program

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import urllib2
 import base64
 import ConfigParser
+import json
 
-# source for the function below:
-# https://wiki.python.org/moin/ConfigParserExamples
+## Helper function for reading config file
+## Source: https://wiki.python.org/moin/ConfigParserExamples
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
@@ -17,20 +18,55 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
-Config = ConfigParser.ConfigParser()
-Config.read('config.ini')
-print Config.sections()
+## Core function for processing query
+## First do concatenation to get the Bing Search API request url
+## Then send the request and get results
+## Return only desired fields, which are 'Title', 'Url', 'Description' in our case
+## Renamed to 'title', 'url', 'desp'
+def processQuery(query):
+    ## Pass in the query string
+    ## Get top 10 results
+    ## Specify the result format to be json
+    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' + query + '%27&$top=10&$format=json'
+    accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
+    headers = {'Authorization': 'Basic ' + accountKeyEnc}
+    req = urllib2.Request(bingUrl, headers = headers)
+    response = urllib2.urlopen(req)
+    
+    ## Get result list
+    result = response.read()
+    ## result contains the json response from Bing. 
+    parsed_result = json.loads(result)['d']['results']
+    parsed_result = [{'title': r['Title'], 'url': r['Url'], 'desp':r['Description']} for r in parsed_result]
+    return parsed_result
 
-# Read accountKey from the config file
-accountKey = ConfigSectionMap("BingAPI")['accountkey']
-print accountKey
+## Prompt for user feedback for each of the Top 10 results
+def getUserFeedback(result):
+    ## Show 10 results in loop and get user feedback
+    print 'See Top 10 results below, and please give us your feedback:'
+    relevant = []
+    has_relev = False
+    return has_relev, relevant 
+ 
+if __name__ == '__main__':
+    ## Open config file
+    Config = ConfigParser.ConfigParser()
+    Config.read('config.ini')
+    # print Config.sections()
 
-bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27gates%27&$top=10&$format=Atom'
+    ## Read accountKey from the config file
+    accountKey = ConfigSectionMap("BingAPI")['accountkey']
+    # print accountKey
 
-accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
-headers = {'Authorization': 'Basic ' + accountKeyEnc}
-req = urllib2.Request(bingUrl, headers = headers)
-response = urllib2.urlopen(req)
-content = response.read()
-#content contains the xml/json response from Bing. 
-print content
+    ## Pass in the query and get the result list
+    query = 'gates'
+    result = processQuery(query)
+    # print out for debugging
+    print json.dumps(result, indent=4, sort_keys=True)
+    
+    has_relev, relevant = getUserFeedback(result)
+    ## If no relevant results, exit the program
+    if not has_relev:
+        exit(0)
+
+    print json.dumps(relevant, indent=4, sort_keys=True)
